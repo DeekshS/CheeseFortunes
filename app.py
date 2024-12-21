a5ac777b815d33192ba51102de4685b108e1d8bd
DeekshS <sivakumardeekshita@gmail.com>
2024-12-21 16:15:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, Response
 
 # Create the app inside a function
 def create_app():
@@ -9,10 +9,21 @@ def create_app():
     def index():
         return render_template('index.html')
 
-    # Add a route to serve the sitemap
     @app.route('/sitemap.xml')
     def sitemap():
-        return send_from_directory(app.root_path, 'sitemap.xml', mimetype='application/xml')
+        # Sitemap XML content
+        sitemap_content = """<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+        <url>
+            <loc>https://cheeseeightball.onrender.com/</loc>
+            <lastmod>2024-12-21T21:46:01+00:00</lastmod>
+        </url>
+        </urlset>"""
+        # Return the sitemap with the correct XML content type
+        return Response(sitemap_content, mimetype='application/xml')
 
     return app
 
@@ -20,4 +31,3 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
-
